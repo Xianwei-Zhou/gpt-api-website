5ee535edeeb0211543b5f981592291da33a561b4
zxw <11911224@mail.sustech.edu.cn>
2023-03-22 16:00:01 +0800

    3/22/2023 增加了发送中的提示

--- static/scripts.js
+++ static/scripts.js
@@ -8,12 +8,12 @@ window.addEventListener("load", () => {
     const messageForm = document.getElementById("messageForm");
     const messages = document.querySelector(".messages");
     const modelSelector = document.getElementById("modelSelector");
-    // addMessage("user", "我", "你是一个AI助手，帮我解决我的需求");
-    // addMessage("user", "（提问示例1）我", "你现在是 [角色]，请 [要做什么]，[描述]。下面我有以下几个问题 [1、2...]。例如：我希望你担任我的私人造型师。我会告诉你我的时尚偏好和体型，你会建议我穿的衣服。你应该只回复你推荐的服装，不要写解释。\n我的第一个要求是:我有一个正式的活动，我需要帮助选择服装。\n我的第二个要求是:我正在普吉岛度假，将去参加一个朋友的私人聚会，我需要帮助选择服装。");
-    // addMessage("user", "（提问示例2）我", "你可以解释[xx]吗，可以举几个例子吗？\n" + "提问方式不唯一，明确角色与需求即可")
     messageForm.addEventListener("submit", async function (event) {
-        event.preventDefault();
         const messageInput = event.target.message;
+        event.preventDefault();
+        if (messageInput.value.trim()) {
+            showToast();
+        }
 
         if (messageInput.value.trim()) {
             addMessage("user", "我", messageInput.value);
@@ -74,7 +74,6 @@ window.addEventListener("load", () => {
         numTokens += 2;
         return numTokens;
     }
-
 
 
     function getPreviousMessages() {
@@ -145,6 +144,14 @@ window.addEventListener("load", () => {
             console.error("Error calling the server: ", error.message);
             return `抱歉，出现了一个问题：${error.message},请稍后重试`;
         }
+    }
+
+    function showToast() {
+        const toast = document.getElementById("toast");
+        toast.style.display = "block";
+        setTimeout(() => {
+            toast.style.display = "none";
+        }, 2000);
     }
 
 });
